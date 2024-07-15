ab609a0cec6791fb89f78fd95c55d9461b0c1513
James Jackson <jamesjackson@Jamess-Air.lan>
2024-07-15 17:51:50 -0400

    added all books api to script and a started a search button variable

---
+++ script/logic.js
@@ -0,0 +1,5 @@
+// added searchButton and assigned it to id="search-button"
+const searchButton = document.getElementById("search-button");
+// added api key for books
+const apiKey = "c915d76c2emsh5807295d7e091e0p12591ajsn9cc36b90575c";
+const apiUrl = "https://all-books-api.p.rapidapi.com/getBooks";
